8c50c3274f376ebbb76dc80b086342c483d165e8
fefoo0204 <gotulook@outlook.es>
2023-09-02 04:02:08 -0300

    Solucion del click

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const divSaludo = document.querySelector("#saludoDiv");
+    const botonSaludar = document.querySelector("#saludarButton");
 
     divSaludo.addEventListener("click", function() {
         alert("Hola! Soy el div");
+    });
+
+    botonSaludar.addEventListener("click", function(event) {
+        alert("Hola!");
+        event.stopPropagation(); // Evita que el evento se propague al div
     });
 });
